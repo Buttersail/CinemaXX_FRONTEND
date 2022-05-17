49d85419e4e70491287eb4a1b92ec71430d61f54
Murat Memis <3605716+Buttersail@users.noreply.github.com>
2022-05-17 15:17:02 +0200

    add movies to show up with title
    working on the details and buy feature

--- js-for-pages/home.js
+++ js-for-pages/home.js
@@ -1,31 +1,53 @@
 import { encode, showPage } from '../utils.js'
 import { SERVER_URL } from '../settings.js'
+import { makeOptions } from '../fetchUtils.js'
 
-let URL = SERVER_URL + '/api/movies'
+let URL = SERVER_URL + '/movies'
 
 export function setupMovieHandlers() {
-  document.getElementById('btn-show-movies').onclick = showMovies
-  document.getElementById('btn-buy').onclick = buy
+  showMovies()
 }
 
 function showMovies() {
   console.log('show me a movie pls')
-  // let movieURL = URL
-  // fetch(movieURL)
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log('does this even work?')
-  //     console.log(data)
-  //   })
-  showMoviePosters()
+  let movieURL = URL
+  let options = makeOptions('GET', false, true)
+
+  fetch(movieURL, options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      console.log('Fetch All Movies')
+      const rows = data
+        .map(
+          (movie) => `
+      <tr>
+          <td><img src="https://image.tmdb.org/t/p/w200${encode(movie.cover)}"></td>
+          <br/>
+          <td>${encode(movie.title)}</td>
+          <br/>
+          <button id="btn-details">Details</button>
+          <br/>
+      </tr>
+      `
+        )
+        .join('\n')
+
+      document.getElementById('info').innerHTML = rows
+      document.getElementById('btn-details').onclick = details
+    })
 }
 
-function buy() {
-  console.log('Does this work?')
-  showPage('page-cinemas')
-}
+function details() {
+  let movieURL = URL + ''
+  let options = makeOptions('GET', false, true)
 
-function showMoviePosters() {
-  document.getElementById('doctor-strange-2').style.visibility = 'visible'
-  document.getElementById('top-gun-2').style.visibility = 'visible'
+  fetch(movieURL, options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Details clicked')
+      const details = data.map((movie) => {
+        console.log(movie.id)
+      })
+    })
 }
