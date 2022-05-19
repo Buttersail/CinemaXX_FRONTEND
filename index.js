8112425d69dc5ddb56a1208aba1eae7ae58fd18b
Simon Roswall <simonroswall@gmail.com>
2022-05-19 13:30:45 +0200

    clean up unused fragments from startcode, reorder menu items

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { renderTemplate, setActive, showPage } from './utils.js'
 import { setupLoginHandlers, logout, updateLoginDependentComponents } from './js-for-pages/login.js'
-import { fetchData } from './js-for-pages/fetchProtectedData.js'
 import {setupCreateScreeningHandlers, makeForm} from './js-for-pages/createScreening.js'
 import { setupMovieHandlers } from './js-for-pages/home.js'
 import {setupCreateAccountHandlers} from './js-for-pages/createAccount.js'
@@ -13,10 +12,6 @@ function renderMenuItems(evt) {
   renderTemplate(id) //This setups the HTML for the page
   switch (id) {
     //Here you can execute JavaScript for the selected page
-    case 'page-secret': {
-      fetchData()
-      break
-    }
     case 'page-home': {
       setupMovieHandlers()
       break
